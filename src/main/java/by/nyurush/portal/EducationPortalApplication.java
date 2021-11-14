10286b652ad64afe2789189b183d3c9445b3187b
Natallia Yurush <natallia.yurush@gmail.com>
2021-11-15 01:42:59 +0300

    Add JWT security

---
+++ src/main/java/by/nyurush/portal/EducationPortalApplication.java
@@ -0,0 +1,12 @@
+package by.nyurush.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationPortalApplication.class, args);
+    }
+}
---
+++ src/main/java/by/nyurush/portal/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package by.nyurush.portal.config;
+
+import by.nyurush.portal.config.jwt.JwtFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/user/*").hasRole("USER")
+                .antMatchers("/register", "/auth").permitAll()
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
